cebe22b4903b19a1f2e3a40fd19fe1a48d9625ce
ninob94 <brkzar@gmail.com>
2016-01-21 23:24:24 +0100

    samo sem dodal fragment moja potovanja

---
+++ app/src/main/java/com/travelerbuddy/feri/travelersbuddy/MojaPotovanjaFragment.java
@@ -0,0 +1,27 @@
+package com.travelerbuddy.feri.travelersbuddy;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class MojaPotovanjaFragment extends Fragment {
+
+
+    public MojaPotovanjaFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_moja_potovanja, container, false);
+    }
+
+}
